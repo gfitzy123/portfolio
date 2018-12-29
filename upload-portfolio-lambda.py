fa348c228b30efcc80bd02fcf8c070df43cc8441
Garrett <gfitzgerald123@gmail.com>
2018-12-29 11:37:36 -0600

    new sns stuff

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,3 +1,4 @@
+import json
 import boto3
 from botocore.client import Config
 import io
@@ -6,17 +7,27 @@ from io import BytesIO as StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
 
-portfolio_bucket = s3.Bucket('portfolio.garrettfitzgerald.com')
-build_bucket = s3.Bucket('portfoliobuild.garrettfitzgerald.com')
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:917733420865:deploy_portfolio_topic')
+    try:
+        portfolio_bucket = s3.Bucket('portfolio.garrettfitzgerald.com')
+        build_bucket = s3.Bucket('portfoliobuild.garrettfitzgerald.com')
 
-portfolio_zip = StringIO()
+        portfolio_zip = StringIO()
 
-build_bucket.download_fileobj('build_portfolio.zip', portfolio_zip)
+        build_bucket.download_fileobj('build_portfolio.zip', portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print('Job done!')
+        topic.publish(Subject="Portfolio Deploy Suceeded", Message="Job complete")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="Job incomplete")
+        raise
